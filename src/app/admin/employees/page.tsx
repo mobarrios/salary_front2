37eea75bfa4c987598d92fc72e830a36a2f5d8cc
Your Name <manu@MBP-de-Manu.fibertel.com.ar>
2024-06-11 16:59:18 -0300

    first commit

---
+++ src/app/admin/employees/page.tsx
@@ -0,0 +1,49 @@
+export default function Employees() {
+  return (
+    <div className="mt-5">
+        <h1>Employees</h1>
+        <table className="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    <nav aria-label="Page navigation example">
+        <ul className="pagination">
+            <li className="page-item disabled">
+                <a className="page-link" href="#" >Previous</a>
+            </li>
+            <li className="page-item"><a className="page-link" href="#">1</a></li>
+            <li className="page-item"><a className="page-link" href="#">2</a></li>
+            <li className="page-item"><a className="page-link" href="#">3</a></li>
+            <li className="page-item">
+                <a className="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
+    </div>
+  )};
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,54 @@
+// components/Navbar.js
+
+const Navbar = () => {
+  return (
+<nav className="navbar navbar-expand-lg shadow-sm ">
+  <div className="container-fluid ">
+    <a className="navbar-brand" href="#"><h3>Salary</h3></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">
+           Dashboard
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/admin/employees">Employees</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Administration
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Users</a></li>
+            <li><a className="dropdown-item" href="#">Roles</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     <i className="bi bi-person-circle"></i>
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Profile</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+
+      </ul>
+      {/* <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-success" type="submit">Search</button>
+      </form> */}
+    </div>
+  </div>
+</nav>
+  );
+};
+
+export default Navbar;
