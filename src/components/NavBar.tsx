5954f534ab6093a023c4fe911a8e9fa9a4b8be97
leandro rocha <leandrorocha@MacBook-Air-de-leandro.local>
2024-06-12 20:03:35 -0300

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,53 +1,61 @@
-// components/Navbar.js
+'use client';
+import { signOut } from "next-auth/react"
 
 const Navbar = () => {
-  return (
-<nav className="navbar navbar-expand-lg shadow-sm navbar-dark bg-primary ">
-  <div className="container-fluid ">
-    <a className="navbar-brand" href="#"><h3>Salary</h3></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">
-           Dashboard
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/admin/employees">Employees</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Administration
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Users</a></li>
-            <li><a className="dropdown-item" href="#">Roles</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     <i className="bi bi-person-circle"></i>
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Logout</a></li>
-          </ul>
-        </li>
 
-      </ul>
-      {/* <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-success" type="submit">Search</button>
-      </form> */}
-    </div>
-  </div>
-</nav>
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: '/' });
+  }
+
+
+  return (
+    <nav className="navbar navbar-expand-lg shadow-sm navbar-dark bg-primary ">
+      <div className="container-fluid ">
+        <a className="navbar-brand" href="#"><h3>Salary</h3></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Dashboard
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/admin/employees">Employees</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Administration
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Users</a></li>
+                <li><a className="dropdown-item" href="#">Roles</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="bi bi-person-circle"></i>
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Profile</a></li>
+                <li><hr className="dropdown-divider" /></li>
+
+                <li><button className="dropdown-item" onClick={() => handleLogout()}>Logout</button></li>
+              </ul>
+            </li>
+
+          </ul>
+          {/* <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-success" type="submit">Search</button>
+            </form> */}
+        </div>
+      </div>
+    </nav>
   );
 };
 
