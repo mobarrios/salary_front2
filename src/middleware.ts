63fe7d02a243da12dc3664a36fd9bf1145949231
Your Name <manu@MacBook-Pro-de-Manu.local>
2024-08-02 12:37:32 -0300

    fix token

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,33 @@ import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
     const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    console.log('middleware', session)
     //const token = await getServerSession(authOptions)
     //const jwt = token.user.token;
 
-    if (!session) {
+    if (!session) 
+    {
         const url = req.nextUrl.clone();
         url.pathname = '/auth/signin';
         return NextResponse.redirect(url);
+    
+    } else {
+        
+        const requestOptions = {
+                method: 'GET',
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${session.user.token}`
+                }
+            };
+           const response = await fetch(process.env.API_SALARY + `/token/validate`, requestOptions);
+       
+            if (response.status === 401) {
+                 const url = req.nextUrl.clone();
+                 url.pathname = '/auth/signin';
+                 return NextResponse.redirect(url);
+            }
+        return NextResponse.next();       
     }
-    return NextResponse.next();
 }
 
 export const config = {
